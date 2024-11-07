f7714a79aaddddcdff5361a80bf76c94f2a74e87
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2024-11-07 15:25:26 +0100

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,11 +3,11 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "hairdresser",
-  password: "admin",
-  port: 5432,
+  user: {{ team.POSTGRES_USER }},
+  host: {{ team.HOST }},
+  database: {{ project.DB_NAME }},
+  password: {{ team.POSTGRES_PASSWORD }},
+  port: {{ team.POSTGRES_PORT }},
 });
 
 export default pool;
